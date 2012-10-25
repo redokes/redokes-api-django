9bf14a305d96495f1046afe5e91531789bef72bd
Wes Okes <wokes@lamppostgroup.com>
2012-10-25 11:41:49 -0400

    * Added support for PUT and DELETE requests

--- Redokes/redokes/views.py
+++ Redokes/redokes/views.py
@@ -1,8 +1,10 @@
 from redokes.request.Parser import Parser
 from redokes.controller import Front
 from django.http import HttpResponse
-import logging
+from django.http import QueryDict
 
 def route(request, request_string):
+    if request.method == 'PUT' or request.method == 'DELETE':
+        request.POST = QueryDict(request.raw_post_data)
     front_controller = Front.Front(request, request_string)
     return front_controller.run()
