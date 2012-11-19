e5940dab2cd399612f5bd5458966fd1ec1ca556e
Wes Okes <wokes@lamppostgroup.com>
2012-11-19 17:38:54 -0500

    * Bump to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Redokes",
-    version = "0.1.3",
+    version = "0.1.4",
     packages = find_packages(),
     url = "https://github.com/redokes/redokes-api-django",
     description = "Redokes Framework.",
